fd47faf6678c21fcf2b0cadc4282752eda7f4cc5
eduaruuoliveira <ayla.livi@gmail.com>
2024-04-03 12:37:23 -0300

    Criação do FrontEnd

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
+import MarcaView from "../views/MarcaView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
+import CorView from "../views/CorView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +17,21 @@ const router = createRouter({
       path: "/categorias",
       name: "categorias",
       component: CategoriaView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
+    },
+    {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessorioView,
+    },
+    {
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
     },
   ],
 });
